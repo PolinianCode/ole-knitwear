f81d23a0dbaa5544ca60a0914241e7d32d0048af
Danylo Koval <danilkoval2004@gmail.com>
2025-07-26 14:22:59 +0200

    Clear project. Make basic project layout structure with metadata

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import "@/styles/global.css"
+
+export const metadata = {
+    title:"Ole Knitwear",
+    description: "A knitwear clothes from Ukraine"
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="shortcut icon" href="/favicon.ico" />
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
+            </head>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
